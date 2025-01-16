a3de8132102079b942e4b9a3997d31d6634cb88e
Nautilus4K <111691689+Nautilus4K@users.noreply.github.com>
2025-01-16 21:48:31 +0700

    Website update 5
    - Added light - dark modes. Could be switched by clicking a singular button on the navigation bar
    - Changed error page, now shows MDN redirection in a much better way and no more bubble
    - Made many variations of colors now only primary and secondary colors.

---
+++ www/js/general.js
@@ -0,0 +1,54 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+// 0 -> Light
+// 1 -> Dark
+var displayModeValue = getCookie("displayMode")
+if (displayModeValue == "0" || displayModeValue == "") var darkMode = false
+else var darkMode = true
+console.log("DarkMode? " + darkMode)
+
+if (darkMode) {
+    document.documentElement.classList.add('dark');
+    document.getElementById("darklight_img").src = "/assets/dark.png"
+}
+// else {
+//     document.documentElement.classList.toggle('light');
+//     document.getElementById("darklight_img").src = "/assets/light.png"
+// }
+
+function switchMode() {
+    console.log("Switch dark mode from " + darkMode + " to " + !darkMode)
+    darkMode = !darkMode
+    if (darkMode) { 
+        var targetMode = "1"
+        document.documentElement.classList.add('dark');
+        document.getElementById("darklight_img").src = "/assets/dark.png"
+    }
+    else {
+        var targetMode = "0"
+        document.documentElement.classList.remove('dark');
+        document.getElementById("darklight_img").src = "/assets/light.png"
+    }
+    setCookie("displayMode", targetMode, 3650)
+    // document.location.href = document.location.href
+}
