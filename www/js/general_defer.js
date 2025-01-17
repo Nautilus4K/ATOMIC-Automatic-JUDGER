f087f39ac5f572daa0d3a25d3e1093a60952cd69
Nautilus4K <111691689+Nautilus4K@users.noreply.github.com>
2025-01-17 09:11:34 +0700

    Website update 6
    [English]
    - Added credits page
    - Fixed a visual exception when on the error page, the footer's text is in italic
    - Changed a little bit of colors so it looks better in dark and light mode
    - Added shadows on `featurecards` and `frame`s in homepage
    - Made changing between dark and light mode less abrupt and now response faster
    
    [Tiếng Việt]
    - Thêm trang ghi công
    - Sửa lại một ngoại lệ trong giao diện ở chân trang trong trang lỗi là chữ bị nghiêng
    - Tinh chỉnh lại một số màu sắc để nhìn dịu và dễ nhìn hơn
    - Thêm bóng cho class `featurecards` và `frame`
    - Làm cho thay đổi giữa chế độ tối và sáng nhanh hơn và mượt mà hơn, không còn trì trệ hay lag.

--- www/js/general_defer.js
+++ www/js/general_defer.js
@@ -25,12 +25,22 @@ function getCookie(cname) {
 var displayModeValue = getCookie("displayMode")
 if (displayModeValue == "0" || displayModeValue == "") var darkMode = false
 else var darkMode = true
-console.log("DarkMode? " + darkMode)
 
-if (darkMode) {
-  document.documentElement.classList.add('dark');
+if (darkMode) document.getElementById("darklight_img").src = "/assets/dark.png"
+
+function switchMode() {
+    console.log("Switch dark mode from " + darkMode + " to " + !darkMode)
+    darkMode = !darkMode
+    if (darkMode) { 
+        var targetMode = "1"
+        document.documentElement.classList.add('dark');
+        document.getElementById("darklight_img").src = "/assets/dark.png"
+    }
+    else {
+        var targetMode = "0"
+        document.documentElement.classList.remove('dark');
+        document.getElementById("darklight_img").src = "/assets/light.png"
+    }
+    setCookie("displayMode", targetMode, 3650)
+    // document.location.href = document.location.href
 }
-// else {
-//     document.documentElement.classList.toggle('light');
-//     document.getElementById("darklight_img").src = "/assets/light.png"
-// }
