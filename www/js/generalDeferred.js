adcaa7d7d8c964b05bd1e8ef62a343c373b37a48
Nautilus <111691689+Nautilus4K@users.noreply.github.com>
2025-02-27 21:16:50 +0700

    Website update 13
    - Made the navbar (or statsbar) now has usable links
    - Removed random stuffs for testing
    - Made the old dark and light toggle icon looks better with bigger line width
    - Added dark and light mode support

---
+++ www/js/generalDeferred.js
@@ -0,0 +1,24 @@
+if (darkmode == "" || !darkmode || darkmode == "0") {
+    // If cookie is not set, we default to light mode
+    document.getElementById("darklighticon").src = "/assets/light.png"
+} else {
+    document.getElementById("darklighticon").src = "/assets/dark.png"
+}
+
+function modeToggle() {
+    document.documentElement.classList.toggle("dark")
+
+    // Check if the darkmode cookie is something, we change it to anothers
+    // The cookie would exists for estimated time of 1 years
+    const darkmodeState = getCookie("darkmode")
+    console.log(darkmodeState)
+
+    if (darkmodeState == "0" || darkmodeState == "" || !darkmodeState) {
+        // To dark mode
+        setCookie("darkmode", "1", 365) // -> Dark mode
+        document.getElementById("darklighticon").src = "/assets/dark.png"
+    } else {
+        setCookie("darkmode", "0", 365) // -> Light mode
+        document.getElementById("darklighticon").src = "/assets/light.png"
+    }
+}
