42d615d618f15f760d1b70cd68e81c5202494b4f
Nautilus <111691689+Nautilus4K@users.noreply.github.com>
2025-02-28 10:03:34 +0700

    Website update 15
    - Added dropdown menu for user icon
    - Removed redundant `console.log()`s
    - Fixed `error.html`'s lack of defer when adding `generalDeferred.js`

--- main/www/js/generalDeferred.js
+++ main/www/js/generalDeferred.js
@@ -1,3 +1,4 @@
+// Light and dark mode stuffs
 if (darkmode == "" || !darkmode || darkmode == "0") {
     // If cookie is not set, we default to light mode
     document.getElementById("darklighticon").src = "/assets/light.png"
@@ -11,7 +12,7 @@ function modeToggle() {
     // Check if the darkmode cookie is something, we change it to anothers
     // The cookie would exists for estimated time of 1 years
     const darkmodeState = getCookie("darkmode")
-    console.log(darkmodeState)
+    // console.log(darkmodeState)
 
     if (darkmodeState == "0" || darkmodeState == "" || !darkmodeState) {
         // To dark mode
@@ -21,4 +22,18 @@ function modeToggle() {
         setCookie("darkmode", "0", 365) // -> Light mode
         document.getElementById("darklighticon").src = "/assets/light.png"
     }
+}
+
+// User buttons
+let userOptionsShown = false;  // Ensure this is declared outside
+
+function toggleUser() {
+    // console.log("User button clicked");
+    userOptionsShown = !userOptionsShown;
+
+    var buttons = document.querySelectorAll(".dropdownbtn");  // Get all matching elements
+    document.querySelector(".dropdown").classList.toggle("show")
+    buttons.forEach(button => {
+        button.classList.toggle("show")
+    });
 }
